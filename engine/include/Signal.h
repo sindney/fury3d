5ac4a11ab6757e26f6ff780d3fd371df972cd072
sindney <346567691@qq.com>
2016-04-13 10:14:02 +0800

    Update Signal.h

--- engine/include/Signal.h
+++ engine/include/Signal.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <stack>
 #include <vector>
+#include <unordered_map>
 #include <string>
 
 #include "TypeComparable.h"
@@ -29,7 +30,7 @@ namespace fury
 
 	private:
 
-		std::vector<CallbackPair> m_Links;
+		std::unordered_map<size_t, CallbackPair> m_LinkMap;
 
 		std::shared_ptr<void> m_DefaultOwner;
 
@@ -46,6 +47,11 @@ namespace fury
 			m_DefaultOwner = std::static_pointer_cast<void>(std::make_shared<bool>(true));
 		}
 
+		virtual std::type_index GetTypeIndex() const
+		{
+			return m_TypeIndex;
+		}
+
 		size_t GenKey()
 		{
 			if (m_KeyStack.empty())
@@ -60,37 +66,37 @@ namespace fury
 			}
 		}
 
-		virtual std::type_index GetTypeIndex() const
-		{
-			return m_TypeIndex;
-		}
-
 		size_t Connect(void(*f)(Args...))
 		{
-			m_Links.emplace_back(std::make_pair<std::weak_ptr<void>, CallbackFunc>(m_DefaultOwner, f));
-			return GenKey();
+			auto key = GenKey();
+			m_LinkMap.emplace(std::make_pair(key,
+				std::make_pair<std::weak_ptr<void>, CallbackFunc>(m_DefaultOwner, f)));
+			return key;
 		}
 
 		template <class Reciver>
 		size_t Connect(const std::shared_ptr<Reciver> &reciver, void(Reciver::*f)(Args ...))
 		{
+			auto key = GenKey();
 			auto rawPtr = reciver.get();
 			CallbackFunc forward = [rawPtr, f](Args&&... args)
 			{
 				(rawPtr->*f)(std::forward<Args>(args)...);
 			};
 
-			m_Links.emplace_back(std::make_pair(std::static_pointer_cast<void>(reciver), forward));
+			m_LinkMap.emplace(std::make_pair(key,
+				std::make_pair(std::static_pointer_cast<void>(reciver), forward)));
 
-			return GenKey();
+			return key;
 		}
 
 		bool Disconnect(size_t key)
 		{
-			if (key < m_Links.size())
+			auto it = m_LinkMap.find(key);
+			if (it != m_LinkMap.end())
 			{
 				m_KeyStack.push(key);
-				m_Links.erase(m_Links.begin() + key);
+				m_LinkMap.erase(it);
 				return true;
 			}
 			else
@@ -105,22 +111,23 @@ namespace fury
 				m_KeyStack.pop();
 
 			m_CurrentKey = 0;
-			m_Links.clear();
+			m_LinkMap.clear();
 		}
 
 		void Emit(Args&&... args)
 		{
-			for (size_t i = 0; i < m_Links.size(); i++)
+			std::vector<typename std::unordered_map<size_t, CallbackPair>::iterator> tobeRemoved;
+
+			for (auto it = m_LinkMap.begin(); it != m_LinkMap.end(); ++it)
 			{
-				auto pair = m_Links[i];
-				if (pair.first.expired())
-				{
-					m_Links.erase(m_Links.begin() + i);
-					i--;
-					continue;
-				}
-				pair.second(std::forward<Args>(args)...);
+				if (it->second.first.expired())
+					tobeRemoved.push_back(it);
+				else
+					it->second.second(std::forward<Args>(args)...);
 			}
+
+			for (auto it = tobeRemoved.begin(); it != tobeRemoved.end(); ++it)
+				m_LinkMap.erase(*it);
 		}
 	};
 }
