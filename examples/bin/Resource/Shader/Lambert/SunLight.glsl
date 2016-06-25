0bc28f9d9feaedd2c5d7a8ece34782240ce04739
sindney <sindneymail@gmail.com>
2016-06-25 14:39:41 +0800

    SunLight.glsl

--- examples/bin/Resource/Shader/Lambert/SunLight.glsl
+++ examples/bin/Resource/Shader/Lambert/SunLight.glsl
@@ -80,7 +80,7 @@ void main()
 	vec4 shadowCoord = shadow_matrix * vec4(vs_surface_pos, 1.0);
 	shadowCoord = shadowCoord / shadowCoord.w;
 	float depth = shadowCoord.z - bias;
-	fragment_output *= depth > 1.0 ? 0.0 : float(depth < texture(shadow_buffer, shadowCoord.xy).x);
+	fragment_output *= depth > 1.0 ? 1.0 : float(depth < texture(shadow_buffer, shadowCoord.xy).x);
 #endif
 }
 
